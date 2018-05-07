c4670ef2917893026ebab5a1f4157d61f2aa8dea
mariolucin <Lucin2000@hotmail.com>
2018-05-07 09:27:43 +0000

    Refactored interfaces

--- rvs/include/rvsmodule.h
+++ rvs/include/rvsmodule.h
@@ -8,46 +8,31 @@
 #ifndef RVSMODULE_H_
 #define RVSMODULE_H_
 
-extern "C"
+class rvsif0;
+class rvsif1;
+
+class rvsmodule
 {
+public:
+	static rvsmodule* create(const char* modulename);
+	static int        destroy(rvsmodule* pmodule);
+	virtual void*     get_interface(int);
 
-extern void  rvs_module_get_version(int* Major, int* Minor, int* Revision);
-extern char* rvs_module_get_name(void);
-extern char* rvs_module_get_description(void);
-extern int   rvs_module_has_interface(int InterfaceID);
+protected:
+	rvsmodule();
+	virtual ~rvsmodule();
+	int init_interfaces(void);
 
-extern int   rvs_module_init(void);
-extern int   rvs_module_run(int Argc, char* Argv[]);
-extern char* rvs_module_get_errstring(void);
-extern int   rvs_module_get_errint(void);
-extern int   rvs_module_terminate(void);
+protected:
+	void* psolib;
+	rvsif0* pif0;
+	rvsif1* pif1;
 
-// define function pointer types to ease late binding usage
-typedef void  (*t_voidintpintpintp)(int* Major, int* Minor, int* Revision);
-typedef char* (*t_charpvoid)(void);
-typedef int   (*t_intvoid)(void);
-typedef int   (*t_intint)(int);
-typedef int   (*t_intintcharpp)(int Argc, char* Argv[]);
 
-typedef struct
-{
-	t_voidintpintpintp rvs_module_get_version;
-	t_charpvoid        rvs_module_get_name;
-	t_charpvoid        rvs_module_get_description;
-	t_intint           rvs_module_has_interface;
-} RVSIF0;
+};
 
-typedef struct
-{
-	t_intvoid         rvs_module_init;
-	t_intintcharpp    rvs_module_run;
-	t_charpvoid       rvs_module_get_errstring;
-	t_intvoid         rvs_module_get_errint;
-	t_intvoid         rvs_module_terminate;
 
-} RVSIF1;
 
-}
 
 
 #endif /* RVSMODULE_H_ */
--- rvs/src/rvs.cpp
+++ rvs/src/rvs.cpp
@@ -9,95 +9,33 @@
 #include <stdio.h>
 #include <iostream>
 #include <dlfcn.h>
-//#include "gpup.h"
+
+#include "rvsif0.h"
+#include "rvsif1.h"
 #include "rvsmodule.h"
 
-using namespace std;
 
-typedef int (*t_libmain_func)(int, char**);
-int rvs_module_init_interface0(void* pLib, RVSIF0* pIF);
+using namespace std;
 
 
 int main(int Argc, char**Argv) {
 	cout << "Hello from RVS..." << endl; // prints !!!Hello World!!!
 
-	RVSIF0 if0;
-//	RVSIF1 if1;
-
-	void *lib_handle;
 
 
-	lib_handle = dlopen("libgpup.so.1", RTLD_LAZY);
-
-	if (!lib_handle)
-	{
-	   fprintf(stdout, "%s\n", dlerror());
-	   return 1;
-	}
+	rvsmodule* module = rvsmodule::create("libgpup.so.1");
+	rvsif0* pif0 = (rvsif0*)module->get_interface(0);
 
 
-	if( rvs_module_init_interface0(lib_handle, &if0))
-	{
-		   fprintf(stdout, "Could not load IF0\n");
-		   return 1;
-	}
+	fprintf(stdout, "Module: %s\n", pif0->get_name() );
 
-	int major;
-	int minor;
-	int rev;
+	fprintf(stdout, "Description: %s\n", pif0->get_description() );
 
-	(*if0.rvs_module_get_version)(&major, &minor, &rev);
-
-	fprintf(stdout, "Module: %s\n", (*if0.rvs_module_get_name)() );
-
-	fprintf(stdout, "Description: %s\n", (*if0.rvs_module_get_description)() );
-
-	dlclose(lib_handle);
+	rvsmodule::destroy(module);
 
 	return 0;
 }
 
 
-int rvs_module_init_interface0(void* pLib, RVSIF0* pIF)
-{
-	char *error;
-
-	if (!pLib)
-	{
-	   fprintf(stdout, "Module not loaded\n" );
-	   return 1;
-	}
-
-	pIF->rvs_module_get_version = (t_voidintpintpintp)(dlsym(pLib, "rvs_module_get_version"));
-	if ((error = dlerror()) != NULL)
-	{
-	   fprintf(stdout, "%s\n", error);
-	   return 1;
-	}
-
-	pIF->rvs_module_get_name = (t_charpvoid)(dlsym(pLib, "rvs_module_get_name"));
-	if ((error = dlerror()) != NULL)
-	{
-	   fprintf(stdout, "%s\n", error);
-	   return 1;
-	}
-
-	pIF->rvs_module_get_description = (t_charpvoid)(dlsym(pLib, "rvs_module_get_description"));
-	if ((error = dlerror()) != NULL)
-	{
-	   fprintf(stdout, "%s\n", error);
-	   return 1;
-	}
-
-	pIF->rvs_module_has_interface = (t_intint)(dlsym(pLib, "rvs_module_has_interface"));
-	if ((error = dlerror()) != NULL)
-	{
-	   fprintf(stdout, "%s\n", error);
-	   return 1;
-	}
-
-
-	return 0;
-}
 
 
